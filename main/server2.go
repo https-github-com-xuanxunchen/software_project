80f15ddf7e4f0e3988ab664fa7be823e4db85ed4
xuanxunchen <xuanxunchen@tencent.com>
2020-06-06 20:14:57 +0800

    add comments

--- main/server2.go
+++ main/server2.go
@@ -25,17 +25,17 @@ type server2 struct {
 }
 
 func (s *server2) Login(ctx context.Context, in *LoginArgs) (*LoginResult, error) {
-	file1, err := os.OpenFile("table1.txt", os.O_RDWR, os.ModePerm)
+	file1, err := os.OpenFile("table1.txt", os.O_RDWR, os.ModePerm) //打开文件
 	if err != nil {
 		panic(err)
 	}
 
-	scanner := bufio.NewScanner(file1)
+	scanner := bufio.NewScanner(file1) //逐行扫描文件
 	var cnt = 0
 	cntChar := 0
 	for scanner.Scan() {
-		strs := strings.Split(scanner.Text(), " ")
-		fmt.Println(scanner.Text())
+		strs := strings.Split(scanner.Text(), " ") //每行的内容提取出来
+		//fmt.Println(scanner.Text())
 		entry := Entry{
 			Token:    strs[0],
 			Id:       strs[1],
@@ -45,12 +45,12 @@ func (s *server2) Login(ctx context.Context, in *LoginArgs) (*LoginResult, error
 			Mark: strs[4],
 		}
 		cnt++
-		cntChar += len(scanner.Text())
+		cntChar += len(scanner.Text()) //记录总共经过了多少个字符
 		fmt.Println("cntChar = ", cntChar)
-		if entry.Username == in.GetUsername() {
-			if entry.Password == in.GetPassword() {
-				file1.Seek(int64(cntChar-1), 0)
-				file1.WriteString("1")
+		if entry.Username == in.GetUsername() { //用户名和表里的相等
+			if entry.Password == in.GetPassword() { //密码和表里的相等，登录成功
+				file1.Seek(int64(cntChar-1), 0) //文件指针偏移到这里
+				file1.WriteString("1")          //改写登录状态，1表示登录，0表示未登录
 				err1 := Err{
 					Code: 0,
 					Msg:  "no error: login in",
@@ -62,7 +62,7 @@ func (s *server2) Login(ctx context.Context, in *LoginArgs) (*LoginResult, error
 				file1.Close()
 				return &loginResult, nil
 			}
-			err1 := Err{
+			err1 := Err{ //用户名匹配而密码不相等，说明密码错误，登录失败
 				Code: 1,
 				Msg:  "error: wrong password",
 			}
@@ -77,7 +77,7 @@ func (s *server2) Login(ctx context.Context, in *LoginArgs) (*LoginResult, error
 		cntChar += 1
 	}
 
-	//需要在文件追加一行
+	//是新的用户，需要在文件追加一行，表明注册
 	file1.WriteString("\n" + strconv.Itoa(cnt) + " " + strconv.Itoa(cnt) + " " + in.GetUsername() + " " + in.GetPassword() + " " + "1\n")
 
 	err1 := Err{
